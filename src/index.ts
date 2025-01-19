845b35f884f9b50373fb26a7cc012e120ada68c6
Ant2357 <ryu4979@gmail.com>
2025-01-20 00:12:34 +0900

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+// @ts-ignore
+const cowsay = require('cowsay');
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
+app.use('*', cors());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/api/cowsay', (c) => {
+  const text = c.req.query('text') ?? '';
+  const cowsayText = cowsay.say({
+    text: text
+  });
+  return c.json({
+    text: cowsayText
+  });
+  // return c.text(cowsayText);
+});
 
 export default app
